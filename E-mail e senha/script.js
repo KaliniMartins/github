e52e86c98026f43bb99f76a9555de339e74f64f2
KaliniSantos <kalinemartinskm3@gmail.com>
2022-05-08 00:28:49 -0300

    E-mail e senha

---
+++ E-mail e senha/script.js
@@ -0,0 +1,8 @@
+var email = prompt("Digite seu email: ");
+var senha = prompt("Digite sua senha: ");
+
+if (email === "aluno@basesocial.org" && senha === "base@2022") {
+    alert("Você está logado =)");
+} else {
+    alert("E-mail ou senha incorretos =(");
+}
